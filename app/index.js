eb5c7136518a7ab50ee1889195e4c92dbcec4676
Phong Nguyễn <namduong.kh94@gmail.com>
2018-04-22 23:12:35 +0700

    no message

--- app/index.js
+++ app/index.js
@@ -1,1 +1,23 @@
 import CatchFruit from "./CatchFruit";
+
+window.newGame = new CatchFruit({
+    events: {
+        // beforeStart: () => {
+        //     console.log('beforeStart');
+        // },
+        // afterStart: () => {
+        //     console.log('afterStart');
+        // },
+        // beforeStop: () => {
+        //     console.log('beforeStop');
+        // },
+        // afterStop: () => {
+        //     console.log('afterStop');
+        // },
+    },
+    loop: () => {
+        console.log('loop');
+    }
+});
+
+// newGame.start();
--- app/libs/Game.js
+++ app/libs/Game.js
@@ -1,26 +1,47 @@
 export default class Game {
 
-    constructor(options) {
+    /**
+     * 
+     * @param {*} options 
+     */
+    constructor(options = {}) {
         this.playing = false;
         this.options = options;
+        if (!this.options.events) {
+            this.options.events = {};
+        }
     }
 
     start() {
+        if (this.options.events.beforeStart) {
+            this.options.events.beforeStart();
+        }
         if (!this.playing) {
             this.playing = true;
             requestAnimationFrame(this.loop.bind(this));
         }
+        if (this.options.events.afterStart) {
+            this.options.events.afterStart();
+        }
     }
 
     stop() {
+        if (this.options.events.beforeStop) {
+            this.options.events.beforeStop();
+        }
         this.playing = false;
+        if (this.options.events.afterStop) {
+            this.options.events.afterStop();
+        }
     }
 
     loop() {
         if (!this.playing) {
             return;
         }
-        console.log('game loop');
+        if (this.options.loop) {
+            this.options.loop();
+        }
         requestAnimationFrame(this.loop.bind(this));
     }
 }
