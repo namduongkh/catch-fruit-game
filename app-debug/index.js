30162e23b6ba9d6149afa2846920d8e419527888
Phong Nguyễn <namduong.kh94@gmail.com>
2018-04-25 13:56:24 +0700

    Phong Nguyễn’s comment:

--- app-debug/index.js
+++ app-debug/index.js
@@ -4,17 +4,18 @@ import GameScreen from "../app/libs/GameScreen";
 
 let gameScreen = new GameScreen('gameScreen', 320, 400);
 
-let fruit1 = new ScreenObjectExample(gameScreen.ctx, 0, -50, 50, 50, { name: "Fruit1", id: "fruit1" });
-let fruit2 = new ScreenObjectExample(gameScreen.ctx, 120, -50, 50, 50, { name: "Fruit2", id: "fruit2" });
-let scoreBoard = new ScreenObjectExample(gameScreen.ctx, 120, 20, 50, 50, { name: "Score board", id: "scoreBoard" });
-let basket = new ScreenObjectExample(gameScreen.ctx, 100, 300, 100, 100, { name: "Basket", id: "basket1" });
+let fruit1 = new ScreenObjectExample(gameScreen.ctx, 0, -50, 50, 50, { name: "Fruit1" });
+let fruit2 = new ScreenObjectExample(gameScreen.ctx, 120, -50, 50, 50, { name: "Fruit2" });
+let fruit3 = new ScreenObjectExample(gameScreen.ctx, 200, -50, 50, 50, { name: "Fruit3" });
+let scoreBoard = new ScreenObjectExample(gameScreen.ctx, 120, 20, 50, 50, { name: "Score board" });
+let basket = new ScreenObjectExample(gameScreen.ctx, 100, 300, 100, 100, { name: "Basket" });
 
 let _game = new CatchFruit({
     ctx: gameScreen.ctx,
     screen: gameScreen.screen,
     noop: gameScreen.noop,
     basket,
-    fruits: [fruit1, fruit2],
+    fruits: [fruit1, fruit2, fruit3],
     scoreBoard: scoreBoard,
     catched: [new ScreenObjectExample(), new ScreenObjectExample()],
     loopMs: 0
